284aff291609e547ad62e178ddd4fdda027100cc
REN Hao <ren.hao.invictus@gmail.com>
2013-10-15 12:36:11 +0200

    adjust indent

--- week0/part_0.rb
+++ week0/part_0.rb
@@ -1,34 +1,34 @@
 def sum(array)
-	if array.length == 0
-		0
-	else
-		sum = 0
-		for i in array do
-			sum += i
-		end
-		sum
-	end
+  if array.length == 0
+    0
+  else
+    sum = 0
+    for i in array do
+      sum += i
+    end
+    sum
+  end
 end
 
 def max_2_sum(array)
-	if array.length == 0
-		0
-	elsif array.length == 1
-		array.first
-	else
-		sort = array.sort! {|x,y| y <=> x }
-		#sort = array.sort!.reverse
-		sort[0]+sort[1]
-	end
+  if array.length == 0
+    0
+  elsif array.length == 1
+    array.first
+  else
+    sort = array.sort! {|x,y| y <=> x }
+    #sort = array.sort!.reverse
+    sort[0]+sort[1]
+  end
 end
 
 def sum_to_n?(array, n)
-	if array.length == 0 && n == 0
-		true
-	elsif array.length == 1
-		false
-	else
-		array.combination(2).to_a.each { |x| if sum(x) == n; return true end}
-		false
+  if array.length == 0 && n == 0
+    true
+  elsif array.length == 1
+    false
+  else
+    array.combination(2).to_a.each { |x| if sum(x) == n; return true end}
+    false
 end
 end
