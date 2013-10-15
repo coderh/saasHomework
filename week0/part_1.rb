60c1eb80b203ce00f3ded1c41d8e9d0319d1123d
REN Hao <ren.hao.invictus@gmail.com>
2013-10-15 14:43:44 +0200

    part2 finished

--- week0/part_1.rb
+++ week0/part_1.rb
@@ -1,1 +1,19 @@
-def
+def hello(name)
+  "Hello, " + name
+end
+
+def starts_with_consonant?(s)
+  if s == '' 
+    false
+  else
+    !!(s.downcase =~ /^[b-df-hj-np-tv-z]/)
+  end
+end
+
+def binary_multiple_of_4?(s)
+  if s !~ /^(0|1)+$/ || s == '0'
+    false
+  else
+    s.to_i(2) % 4 == 0
+  end
+end
